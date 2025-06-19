50bb283824bd4b666abdf110ff70670bf4a20371
jacosy <longoria.you@gmail.com>
2025-06-19 23:19:48 +0800

    Add /api prefix to the API endpoints

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ func main() {
 	apiCfg := &apiConfig{}
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(prefixHandler))
 
-	serveMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	serveMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	serveMux.HandleFunc("GET /metrics", apiCfg.MetricsHandler)
-	serveMux.HandleFunc("POST /reset", apiCfg.ResetMetricsHandler)
+	serveMux.HandleFunc("GET /api/metrics", apiCfg.MetricsHandler)
+	serveMux.HandleFunc("POST /api/reset", apiCfg.ResetMetricsHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
