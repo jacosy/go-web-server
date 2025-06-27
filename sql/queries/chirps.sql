5879ad9e6c370760e980cee4043150602bde1c87
jacosy <longoria.you@gmail.com>
2025-06-27 22:21:33 +0800

    Add the create chirp SQL script

---
+++ sql/queries/chirps.sql
@@ -0,0 +1,6 @@
+-- name: CreateChirp :one
+INSERT INTO chirps (id, user_id, body, created_at, updated_at)
+VALUES (
+    gen_random_uuid(), $1, $2, NOW(), NOW()
+)
+RETURNING id, user_id, body, created_at, updated_at;
