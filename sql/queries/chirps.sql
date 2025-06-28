56c2fdde0d9096b166f54929e0ddb568ac1e5270
jacosy <longoria.you@gmail.com>
2025-06-28 20:26:00 +0800

    Add GetAllChirps & GetChirpByID SQL queries

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -4,3 +4,14 @@ VALUES (
     gen_random_uuid(), $1, $2, NOW(), NOW()
 )
 RETURNING id, user_id, body, created_at, updated_at;
+
+
+-- name: GetAllChirps :many
+SELECT id, user_id, body, created_at, updated_at
+FROM chirps
+ORDER BY created_at;
+
+-- name: GetChirpByID :one
+SELECT id, user_id, body, created_at, updated_at
+FROM chirps
+WHERE id = $1;
