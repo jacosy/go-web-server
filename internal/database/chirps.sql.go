a49180e54b4ae241344f94520d0733ccaab6369e
jacosy <longoria.you@gmail.com>
2025-06-28 20:27:48 +0800

    Use sqlc to generate GetAllChirps & GetChirpByID methods

--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -36,3 +36,57 @@ func (q *Queries) CreateChirp(ctx context.Context, arg CreateChirpParams) (Chirp
 	)
 	return i, err
 }
+
+const getAllChirps = `-- name: GetAllChirps :many
+SELECT id, user_id, body, created_at, updated_at
+FROM chirps
+ORDER BY created_at
+`
+
+func (q *Queries) GetAllChirps(ctx context.Context) ([]Chirp, error) {
+	rows, err := q.db.QueryContext(ctx, getAllChirps)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Chirp
+	for rows.Next() {
+		var i Chirp
+		if err := rows.Scan(
+			&i.ID,
+			&i.UserID,
+			&i.Body,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getChirpByID = `-- name: GetChirpByID :one
+SELECT id, user_id, body, created_at, updated_at
+FROM chirps
+WHERE id = $1
+`
+
+func (q *Queries) GetChirpByID(ctx context.Context, id uuid.UUID) (Chirp, error) {
+	row := q.db.QueryRowContext(ctx, getChirpByID, id)
+	var i Chirp
+	err := row.Scan(
+		&i.ID,
+		&i.UserID,
+		&i.Body,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+	)
+	return i, err
+}
