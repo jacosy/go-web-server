111fd48a5fbecc18390de59b1eb3a40d4f48923e
jacosy <longoria.you@gmail.com>
2025-07-07 18:47:32 +0800

    Update the UserResponse and LoginRequest models

--- models.go
+++ models.go
@@ -18,9 +18,11 @@ type UserResponse struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Email     string    `json:"email"`
+	Token     string    `json:"token"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email            string `json:"email"`
+	Password         string `json:"password"`
+	ExpiresInSeconds int    `json:"expires_in_seconds,omitempty"`
 }
